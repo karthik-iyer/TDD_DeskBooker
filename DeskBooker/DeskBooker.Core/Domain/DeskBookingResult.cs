f2249a348bb9ec6d15face08098c7ad9e49964d5
Karthik.Iyer <Karthik.Iyer@CLK-6VLWZY2>
2020-12-20 00:29:34 -0600

    Initial commit

---
+++ DeskBooker/DeskBooker.Core/Domain/DeskBookingResult.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DeskBooker.Core.Domain
+{
+    public class DeskBookingResult
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; internal set; }
+        public string Email { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
